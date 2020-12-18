5806b883e810a297131cb73e54335783910e3351
sarp-u <ugursarp1798@gmail.com>
2020-12-18 19:16:11 +0300

    Zamanla içerisine minimax'i de koyacağız umarım

--- Projects/Denemeler/tic-tac-toe/xox.py
+++ Projects/Denemeler/tic-tac-toe/xox.py
@@ -6,8 +6,9 @@ LINE = (0, 100, 100)
 WHITE = (240, 240, 240)
 BLACK = (30, 30, 30)
 
-xOxList = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
+xOxList = ['0', '1', '2', '3', '4', '5', '6', '7', '8']
 isFull = [False, False, False, False, False, False, False, False, False]
+counter = 0
 
 pygame.init()
 screen = pygame.display.set_mode((600, 600))
@@ -87,83 +88,90 @@ def player2(pos_x, pos_y):
     # Birinci sütun
     global counter
     if pos_x <= 190 and pos_y <= 190 and not isFull[0]:
-        pygame.draw.circle(screen, BLACK, (100, 100), 70, 5)
+        pygame.draw.circle(screen, BLACK, (100, 100), 70, 8)
         xOxList[0] = 'O'
         isFull[0] = True
         counter += 1
     if pos_x <= 190 and (190 < pos_y < 380) and not isFull[1]:
-        pygame.draw.circle(screen, BLACK, (100, 300), 70, 5)
+        pygame.draw.circle(screen, BLACK, (100, 300), 70, 8)
         xOxList[1] = 'O'
         isFull[1] = True
         counter += 1
     if pos_x <= 190 and (380 < pos_y < 570) and not isFull[2]:
-        pygame.draw.circle(screen, BLACK, (100, 500), 70, 5)
+        pygame.draw.circle(screen, BLACK, (100, 500), 70, 8)
         xOxList[2] = 'O'
         isFull[2] = True
         counter += 1
     # İkinci sütun
     if (190 < pos_x <= 380) and pos_y <= 190 and not isFull[3]:
-        pygame.draw.circle(screen, BLACK, (300, 100), 70, 5)
+        pygame.draw.circle(screen, BLACK, (300, 100), 70, 8)
         xOxList[3] = 'O'
         isFull[3] = True
         counter += 1
     if (190 < pos_x <= 380) and (190 < pos_y < 380) and not isFull[4]:
-        pygame.draw.circle(screen, BLACK, (300, 300), 70, 5)
+        pygame.draw.circle(screen, BLACK, (300, 300), 70, 8)
         xOxList[4] = 'O'
         isFull[4] = True
         counter += 1
     if (190 < pos_x <= 380) and (380 < pos_y < 570) and not isFull[5]:
-        pygame.draw.circle(screen, BLACK, (300, 500), 70, 5)
+        pygame.draw.circle(screen, BLACK, (300, 500), 70, 8)
         xOxList[5] = 'O'
         isFull[5] = True
         counter += 1
     # Üçüncü sütun
     if (380 < pos_x <= 570) and pos_y <= 190 and not isFull[6]:
-        pygame.draw.circle(screen, BLACK, (500, 100), 70, 5)
+        pygame.draw.circle(screen, BLACK, (500, 100), 70, 8)
         xOxList[6] = 'O'
         isFull[6] = True
         counter += 1
     if (380 < pos_x <= 570) and (190 < pos_y < 380) and not isFull[7]:
-        pygame.draw.circle(screen, BLACK, (500, 300), 70, 5)
+        pygame.draw.circle(screen, BLACK, (500, 300), 70, 8)
         xOxList[7] = 'O'
         isFull[7] = True
         counter += 1
     if (380 < pos_x <= 570) and (380 < pos_y < 570) and not isFull[8]:
-        pygame.draw.circle(screen, BLACK, (500, 500), 70, 5)
+        pygame.draw.circle(screen, BLACK, (500, 500), 70, 8)
         xOxList[8] = 'O'
         isFull[8] = True
         counter += 1
 
 
 def win_condition():
+    # sütun
+    if xOxList[0] == xOxList[1] == xOxList[2]:
+        return True
+    if xOxList[3] == xOxList[4] == xOxList[5]:
+        return True
+    if xOxList[6] == xOxList[7] == xOxList[8]:
+        return True
+
+    # satır
+    if xOxList[0] == xOxList[3] == xOxList[6]:
+        return True
+    if xOxList[1] == xOxList[4] == xOxList[7]:
+        return True
+    if xOxList[2] == xOxList[5] == xOxList[8]:
+        return True
+
+    # diagonal
+    if xOxList[0] == xOxList[4] == xOxList[8]:
+        return True
+    if xOxList[2] == xOxList[4] == xOxList[6]:
+        return True
+
+    return False
+
+def anounce_winner():
     if counter % 2 == 1:
         winner = 'X'
     else:
         winner = 'O'
-    # sütun
-    if xOxList[0] == xOxList[1] == xOxList[2]:
-        print(winner + ' kazandı')
-    if xOxList[3] == xOxList[4] == xOxList[5]:
-        print(winner + ' kazandı')
-    if xOxList[6] == xOxList[7] == xOxList[8]:
-        print(winner + ' kazandı')
-
-    # satır
-    if xOxList[0] == xOxList[3] == xOxList[6]:
-        print(winner + ' kazandı')
-    if xOxList[1] == xOxList[4] == xOxList[7]:
-        print(winner + ' kazandı')
-    if xOxList[2] == xOxList[5] == xOxList[8]:
-        print(winner + ' kazandı')
-
-    # diagonal
-    if xOxList[0] == xOxList[4] == xOxList[8]:
-        print(winner + ' kazandı')
-    if xOxList[2] == xOxList[4] == xOxList[6]:
-        print(winner + ' kazandı')
-
-
-counter = 0
+    pygame.draw.rect(screen, TEAL, (208, 208, 180, 180))
+    font = pygame.font.SysFont(None, 24)
+    img = font.render(winner + ' Kazandı', True, WHITE)
+    screen.blit(img, (260, 250))
+    img2 = font.render('Restart (R)', True, WHITE)
+    screen.blit(img2, (260, 330))
 
 
 def game(pos_x, pos_y):
@@ -172,15 +180,33 @@ def game(pos_x, pos_y):
         player1(pos_x, pos_y)
     else:
         player2(pos_x, pos_y)
-    win_condition()
+    isWon = win_condition()
+    if isWon:
+        for i in range(9):
+            isFull[i] = True
+        anounce_winner()
+
+
+def play_again():
+    global counter
+    screen.fill(TEAL)
+    drawing_lines()
+    for i in range(9):
+        xOxList[i] = str(i)
+        isFull[i] = False
+        counter = 0
 
 
 drawing_lines()
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.MOUSEBUTTONUP:
             pos = pygame.mouse.get_pos()
             game(pos[0], pos[1])
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_r:
+                play_again()
         if event.type == pygame.QUIT:
             sys.exit()
     pygame.display.update()
